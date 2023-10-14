8cf8f662db8da0775fe430200732442bbfe2ced5
lud-wj <ludovic.demblans@wttj.co>
2023-10-14 18:12:12 +0200

    update params from blocks

--- src/components/ConfigFrom.tsx
+++ src/components/ConfigFrom.tsx
@@ -3,7 +3,11 @@ import configSchema from '../schema.json'
 import { Field } from '@welcome-ui/field'
 import { Select } from '@welcome-ui/select'
 import { Button } from '@welcome-ui/button'
-
+import { InputText } from '@welcome-ui/input-text'
+import { Label } from '@welcome-ui/label'
+import { Flex } from '@welcome-ui/flex'
+import { TrashIcon } from '@welcome-ui/icons'
+import { flexShrink } from '@xstyled/styled-components'
 console.clear()
 
 interface StepConfig {
@@ -29,13 +33,13 @@ function importDef(confSchema, key: string) {
 function importParams(paramProperties) {
   const params = {}
   for (const [key, value] of Object.entries(paramProperties)) {
-    params[key] = importParam(value)
+    params[key] = importParam(value, key)
   }
   return params
 }
 
-function importParam(paramDef) {
-  const param = { type: paramDef.type }
+function importParam(paramDef, key) {
+  const param = { ...paramDef, key }
   switch (param.type) {
     case void 0:
       if (paramDef.$ref == '#/$defs/transform_function') {
@@ -87,8 +91,7 @@ function deriveSetter(setter, path: derivePath, index: number = 0) {
       const subsetter = typeof update === 'function' ? update : () => update
       setter((obj) => ({ ...obj, [key]: subsetter(obj[key]) }))
     }
-  }
-  else if (typeof key === 'number') {
+  } else if (typeof key === 'number') {
     derived = function (update) {
       const subsetter = typeof update === 'function' ? update : () => update
       setter((array) => {
@@ -97,8 +100,7 @@ function deriveSetter(setter, path: derivePath, index: number = 0) {
         return copy
       })
     }
-  }
-  else {
+  } else {
     throw new Error(
       'unsupported path key type: ' + typeof key + ': ' + JSON.stringify(path)
     )
@@ -108,7 +110,7 @@ function deriveSetter(setter, path: derivePath, index: number = 0) {
 
 function ConfigForm() {
   const configID = 'some-uuid'
-  const cacheVsn = '1'
+  const cacheVsn = '2'
   const cacheKey = `config-${configID}-${cacheVsn}`
 
   const [config, setConfig] = useState(() => {
@@ -117,6 +119,7 @@ function ConfigForm() {
   })
 
   useEffect(() => {
+
     localStorage.setItem(cacheKey, JSON.stringify(config))
   }, [config])
 
@@ -164,14 +167,26 @@ function StepPicker({ add }) {
   )
 }
 
-function listPartition(list, predicate) {
+function listPartition(list, predicate, valueFun) {
+
   const truthy = []
   const falsy = []
   list.forEach((item) => {
-    if (predicate(item)) truthy.push(item)
-    else falsy.push(item)
+    if (predicate(item)) truthy.push(valueFun(item))
+    else falsy.push(valueFun(item))
   })
+
+
   return [truthy, falsy]
+}
+
+function objectPartition(obj, predicate) {
+
+  const [truthy, falsy] = listPartition(Object.entries(obj), predicate)
+
+
+
+  return [Object.fromEntries(truthy), Object.fromEntries(falsy)]
 }
 
 function isBlockInput(paramDef) {
@@ -183,23 +198,60 @@ function isBlockInput(paramDef) {
 function StepConfig({ stepConfig, setParams, deleteStep }) {
   const { type, params } = stepConfig
   const def = tstepIndex[type]
-  const [blockInputs, inlineInputs] = listPartition(
+
+  let [blockInputs, inlineInputs] = listPartition(
     Object.entries(def.params),
-    ([key, value]) => isBlockInput(value)
+    ([key, value]) => isBlockInput(value),
+    ([key, value]) => ({ ...value, key: key })
   )
-  console.log(`blockInputs`, blockInputs)
-  console.log(`inlineInputs`, inlineInputs)
+  blockInputs = blockInputs.map(paramDef => makeInput(paramDef, params, setParams))
+  inlineInputs = inlineInputs.map(paramDef => makeInput(paramDef, params, setParams))
+
+
   return (
     <div>
-      <h2>{def.title}</h2>
-      <Button onClick={deleteStep} size="xxs">
-        X
-      </Button>
+      <Flex align="center">
+        <Label p="5">{def.title}</Label>
+        <Button onClick={deleteStep} size="xxs"><TrashIcon /></Button>
+      </Flex>
 
+      <Flex direction="row" justify="flex-start" align="flext-end" wrap="wrap" border='1px solid red'>
+        {inlineInputs}
+      </Flex>
+
+      <Flex>
+        {blockInputs}
+      </Flex>
       <pre>{JSON.stringify(params, null, '  ')}</pre>
       <pre>{JSON.stringify(def, null, '  ')}</pre>
     </div>
   )
+}
+
+function makeInput(paramDef, params, setParams) {
+  const setValue = deriveSetter(setParams, paramDef.key)
+  switch (paramDef.type) {
+    case 'string':
+      return makeInputText(paramDef, params, setValue)
+    default:
+      throw new Error('unhandled paramDef.type: ' + paramDef.type)
+  }
+}
+function makeInputText(paramDef, params, changeParamValue) {
+
+  const [value, setValue] = useState(params[paramDef.key])
+
+  useEffect(() => {
+    console.log(`call change with value`, value)
+    changeParamValue(value)
+  }, [value])
+
+  return <Flex key={paramDef.key} align='center' direction='row' flexGrow="0" flexShrink="0" flexBasis="content">
+    <Label pl="6" pr="3">{paramDef.key + ': '}</Label>
+    {paramDef.title} {paramDef.title} {paramDef.title}
+    <InputText name="firstName" placeholder="Boaty" value={value} onChange={evt => setValue(evt.target.value)} />
+  </Flex >
+
 }
 
 export default ConfigForm
